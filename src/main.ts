6b07af91aa78e3488ef81af9181ccef58a0b4877
thaibucher <haibut41@gmail.com>
2026-05-05 18:16:31 -0500

    Upgrade to Angular 21

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode, provideZoneChangeDetection } from '@angular/core';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { platformBrowser } from '@angular/platform-browser';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowser()
+  .bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()] })
   .catch(err => console.error(err));
